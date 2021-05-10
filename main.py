967847ff7156cfc63064b1bf203e0bcbd423b478
Ubuntu <ubuntu@ip-172-31-11-170.eu-west-1.compute.internal>
2021-05-10 01:55:25 +0000

    debug

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import sys
 from aioquant import quant
 
 def initialize():
-    from strategy.strategy_test import MyStrategy
+    from strategy.strategy_grid import MyStrategy
     MyStrategy()
 
 if __name__ == "__main__":
     config_file = sys.argv[1]
     quant._initialize(config_file)
     initialize()
-    quant.start(config_file)
+    quant.start()
